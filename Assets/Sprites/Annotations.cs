584f39cb42d9c4aa619b10141e6446370715b8da
Andrew Eveld <eveldaj@hendrix.edu>
2021-02-03 21:47:28 -0600

    Added Bowling ball that bounces on impact

--- Assets/Sprites/Annotations.cs
+++ Assets/Sprites/Annotations.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Annotations : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    // Bear Picture: https://opengameart.org/content/bear-0
+    // Bowling ball picture: https://creazilla.com/nodes/79396-bowling-ball-clipart
     void Start()
     {
         
