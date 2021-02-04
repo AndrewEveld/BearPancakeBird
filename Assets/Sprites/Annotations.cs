99b86d209a2e38ab9db1c47ead86eac7a7c8a20e
ianc02 <77699025+ianc02@users.noreply.github.com>
2021-02-04 10:02:14 -0600

    Bigger text and added sources to Annotations script

--- Assets/Sprites/Annotations.cs
+++ Assets/Sprites/Annotations.cs
@@ -6,6 +6,8 @@ public class Annotations : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    // Background - https://opengameart.org/content/bevouliin-free-mountain-game-background
+    // Pancake - https://www.cleanpng.com/png-pancake-android-p-pixel-2-pixel-art-color-by-numbe-1722375/download-png.html
     // Bear Picture: https://opengameart.org/content/bear-0
     // Bowling ball picture: https://creazilla.com/nodes/79396-bowling-ball-clipart
     void Start()
